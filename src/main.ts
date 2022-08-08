0e8ecdec04ea4449cce4a85ee94d69c55d0b24b7
BIYA Paul <paul@workerly.io>
2022-08-08 09:50:19 +0100

    chore: Add element-plus to the project.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from "vue"
 import router from "@/router"
+import ElementPlus from 'element-plus'
 import App from "./App.vue"
+import 'element-plus/dist/index.css'
 import "animate.css"
 import "./main.css"
 
-createApp(App).use(router).mount("#app")
+createApp(App).use(router).use(ElementPlus).mount("#app")
